0b2f361d6ea87ad6eaa39a37f9dbad0971f3c88d
mrsir <mrsir89@gmail.com>
2022-05-21 09:32:46 +0900

    test

---
+++ src/test/java/com/demo/socket/demosocket/DemoSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.socket.demosocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
